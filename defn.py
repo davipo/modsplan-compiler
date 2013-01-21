c7cfd18b0ac7670c0aafe54b9c5e8a7d49a9ada4
David H Post <post@pobox.com>
2013-01-20 22:12:11 -0600

    First version of defn.py to load .defn specs

--- defn.py
+++ defn.py
@@ -4,42 +4,62 @@
 
 
 import syntax
-
+from grammar import Error
 
 
 class DefnNode:
     
-    def __init__(self, name, args, instructions):
-        self.name = name                    # name of node (nonterminal or terminal)
-        self.args = args                    # list of arguments (children of node)
+    def __init__(self, content, instructions):
+        self.text = None                    # text if terminal node
+        self.args = None                    # list of arguments if nonterminal
         self.instructions = instructions    # list of code generation instructions
+        if isinstance(content, str):
+            self.text = content
+        elif isinstance(content, list):
+            self.args = content
+        else:
+            raise Error().msg('Invalid content for DefnNode %s' % name)
 
 
-class Defn:
-    """ Read and store .defn files (used to generate code from syntax trees)."""
+class Definitions:
+    """ Holds semantic definitions (used to generate code from syntax trees)."""
     
     def __init__(self):
-        self.defns = dict()     # dictionary of definitions
-                                #   key is a list of strings: name followed by args
-                                #   value is a list of instructions
+        self.defns = dict()     # dictionary of definitions: key is itemname, 
+                                #   value is a list of DefnNode for this item
         self.defn_parser = syntax.SyntaxParser('grammars/defn')
 
         
-    def load(self, filename):
-        """ Load definitions from file.
-            Store definitions in self.defns.
-        """
-        
-        # Use a SyntaxParser to load definitions into a syntax tree.
-        self.defn_tree = self.defn_parser.parse(filename)
+    def load(self, langname):
+        """ Load definitions from langname.defn file."""
+        # Use a SyntaxParser to load definitions into a parse tree.
+        self.defn_tree = self.defn_parser.parse(langname + '.defn')
+
+        # Extract definitions from tree
+        for definition in self.defn_tree.findall('definition'):
+            signature = definition.first()
+            sigkind = signature.first()
+            name = sigkind.findtext()
+            if sigkind.name == 'terminal':
+                content = signature.find('STRING').text
+            elif sigkind.name == 'nonterm':
+                content = [arg.findtext() for arg in signature.findall('arg')]
+            else:
+                msg = 'Invalid signature type "%s" in %s definition' % (sigkind.name, name)
+                raise Error().msg(msg)
+            instructions = definition.find('instructions').findall('instruction')
+            ### decode instructions here?
+            self.defns.setdefault(name, []).append(DefnNode(content, instructions))
 
         
-        # Extract definitions from tree into self.defns.
         
         
 
-defs = Defn()
+defs = Definitions()
 
-dfile = 'grammars/base.defn'
-defs.load(dfile)
-defs.defn_tree.show()
+lang = 'grammars/smaller'
+defs.load(lang)
+print defs.defns
+
+
+
