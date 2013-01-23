7c3d8f5f8888a58d733c809a43c0f2c7fb1bb128
David H Post <post@pobox.com>
2013-01-23 06:30:38 -0600

    Rewrote defn.py to lookup defns by signature. Not using DefnNode, may eliminate it later.

--- defn.py
+++ defn.py
@@ -10,18 +10,9 @@ from grammar import Error
 
 class DefnNode:
     
-    def __init__(self, content, instructions):
-#         self.name = name                    # name of node (nonterminal or terminal)
-        self.text = None                    # text, if terminal node, else None
-        self.args = None                    # list of argument names, if nonterminal
+    def __init__(self, instructions):
         self.instructions = instructions    # list of code generation instructions
-        if isinstance(content, str):
-            self.text = content
-        elif isinstance(content, list):
-            self.args = content
-        else:
-            raise Error().msg('Invalid content for DefnNode %s' % name)
-
+        ### any other data? If not, we don't need this class.
 
     def __str__(self):
         if self.text == None:   # nonterminal
@@ -37,10 +28,11 @@ class Definitions:
     def __init__(self, defn_grammar_dir='defn_grammar/'):
         """ Initialize defn parser to prepare for parsing .defn specs."""
         defn_path = os.path.join(defn_grammar_dir, 'defn')
-        self.defn_parser = syntax.SyntaxParser(defn_path)   # use defn.tokens, defn.syntax
-        self.defns = dict()     # dictionary of definitions: key is itemname, 
-                                #   value is a list of DefnNode for this item
         self.defn_tree = None   # parse tree of last definitions loaded; set by load()
+        self.defn_parser = syntax.SyntaxParser(defn_path)   # use defn.tokens, defn.syntax
+        self.defns = dict()     # Dictionary of definitions: 
+                                #   key is signature (list of strings for name and args), 
+                                #   value is a list of instructions for this defn.
         
         
     def load(self, langpath):
@@ -49,36 +41,23 @@ class Definitions:
         self.defn_tree = self.defn_parser.parse(langpath + '.defn')
 
         # Extract definitions from tree
+        self.defns.clear()
         for definition in self.defn_tree.findall('definition'):
-            signature = definition.first()
-            sigkind = signature.first()
-            name = sigkind.findtext()
-            if sigkind.name == 'terminal':
-                content = signature.find('STRING').text
-            elif sigkind.name == 'nonterm':
-                content = [arg.findtext() for arg in signature.findall('child')]
-                ### need to record subtype and index
-            else:
-                msg = 'Invalid signature type "%s" in %s definition' % (sigkind.name, name)
-                raise Error().msg(msg)
-            instructions = definition.find('instructions').findall('instruction')
+            signature = [node.findtext() for node in definition.first().children]
+            ### need to get subtypes
+            instructions = definition.first('instructions')
             ### decode instructions here?
-            self.defns.setdefault(name, []).append(DefnNode(content, instructions))
-
-    
+            self.defns[signature] = instructions.children
+   
+   
     def find(self, source_node):
-        """ Return a definition (DefnNode) matching source_node, or None."""
-        definitions = self.defns.get(source_node.name, [])
-        for defn in definitions:
-            # Does defn match source_node?
-            if source_node.isterminal():
-                match = (defn.text == source_node.text)
-            else:  # nonterminal source_node
-                # Does defn match source_node args?
-                match = (defn.args == [child.name for child in source_node.children])
-            if match:
-                return defn
-        return None     # no match found
+        """ Return a list of instructions for the defn matching source_node, or None."""
+        signature = [source_node.name]
+        if source_node.isterminal():
+            signature += [source_node.text]
+        else:           
+            signature += [child.name for child in source_node.children]
+        return self.defns.get(signature)
     
     
     def show(self):
