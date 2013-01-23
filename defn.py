6037a45f815c6b7182ef87980d42afe29d305e98
David H Post <post@pobox.com>
2013-01-22 20:19:23 -0600

    find() and show() methods added to Definitions class

--- defn.py
+++ defn.py
@@ -12,8 +12,8 @@ class DefnNode:
     
     def __init__(self, content, instructions):
 #         self.name = name                    # name of node (nonterminal or terminal)
-        self.text = None                    # text if terminal node
-        self.args = None                    # list of arguments if nonterminal
+        self.text = None                    # text, if terminal node, else None
+        self.args = None                    # list of argument names, if nonterminal
         self.instructions = instructions    # list of code generation instructions
         if isinstance(content, str):
             self.text = content
@@ -24,12 +24,11 @@ class DefnNode:
 
 
     def __str__(self):
-        s = '<'
-        if self.text == None:
-            s += str(self.args)
-        else:
-            s += self.text
-        return s + '>'
+        if self.text == None:   # nonterminal
+            display = ' '.join(self.args)
+        else:   # terminal
+            display = self.text
+        return '(' + display + ')'
     
 
 class Definitions:
@@ -37,8 +36,8 @@ class Definitions:
     
     def __init__(self, defn_grammar_dir='defn_grammar/'):
         """ Initialize defn parser to prepare for parsing .defn specs."""
-        defn_path = os.path.join(defn_grammar_dir, 'defn')      # defn.tokens, defn.syntax
-        self.defn_parser = syntax.SyntaxParser(defn_path)
+        defn_path = os.path.join(defn_grammar_dir, 'defn')
+        self.defn_parser = syntax.SyntaxParser(defn_path)   # use defn.tokens, defn.syntax
         self.defns = dict()     # dictionary of definitions: key is itemname, 
                                 #   value is a list of DefnNode for this item
 
@@ -65,14 +64,36 @@ class Definitions:
             ### decode instructions here?
             self.defns.setdefault(name, []).append(DefnNode(content, instructions))
 
-        
-        
+    
+    def find(self, source_node):
+        """ Return a definition (DefnNode) matching source_node, or None."""
+        definitions = self.defns[source_node.name]
+        for defn in definitions:
+            # Does defn match source_node?
+            if source_node.isterminal():
+                match = (defn.text == source_node.text)
+            else:  # nonterminal source_node
+                # Does defn match source_node args?
+                match = (defn.args == [child.name for child in source_node.children])
+            if match:
+                return defn
+        return None     # no match found
+    
+    
+    def show(self):
+        """ Return string display of current definitions."""
+        display = ''
+        for name, defns in self.defns.items():
+            namestr = name + ': '
+            indent = ' ' * len(namestr)
+            display += namestr + ('\n' + indent).join(map(str, defns)) + '\n'
+        return display
+    
+
 if __name__ == '__main__':
     defs = Definitions()
-
-    lang = 'grammars/smaller'
+    lang = 'grammars/L0'
     defs.load(lang)
-    print defs.defns
-
+    print defs.show()
 
 
